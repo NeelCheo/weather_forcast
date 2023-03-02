b976a43d36b33e8b3278f06965ad14e698b80a32
NeelCheo <73446815+NeelCheo@users.noreply.github.com>
2023-03-01 19:47:14 -0500

    made the function retieve the weather data for the search location in the US by name (there was no need for the geo coords info)

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 //api variables
-// var geoApi = link; //also make sure it matches US country 
 // var weatherApi = link;
-time = dayjs();
+var time = dayjs();
 // document variables
 var searchInput = $("#search-input");
 var submitBtn = $("#submit-button");
@@ -9,33 +8,48 @@ var searchHistory = $("#search-history");
 var todayInfo = $("#today-info");
 var fiveDayInfo = $("#5-day-info");
 //storage variables
-searchedHistory =[];
+var searchedHistory =[];
 
 // renders the search history on load based on local storage
-renderHistory(searchedHistory);
+// renderHistory(searchedHistory);
 
 // on click function that will handles everything 
 //since the app is mainly displaying the info from the api
-submitBtn.on(click,function(event){
-  event.preventDefault(); 
-  displayInfo();
-  renderHisory();
+submitBtn.on("click",function(event){
+  event.preventDefault();
+  var city = searchInput.val().trim().toLowerCase();
+  city = city.charAt(0).toUpperCase()+city.slice(1);
+  console.log(city);
+  displayInfo(city);
+  // renderHisory();
   //at end clear input
 });
 
 //display the info from the api
-function displayInfo(){
-  function convertNameToGeo(geoApi, searchInput){
-    fetch()
-    .then(function(response){})
+function displayInfo(city){
+  convertNameToData();
+  function convertNameToData(){
+    // DISCLAIMER: it seems the weather api can use by defualt names instead of coordinates but converted it because the assignemnt asked for it 
+    var geoApi = "https://api.openweathermap.org/data/2.5/forecast?q="+city+",US&appid=b117dcb8d3b6343f236365cabe485360";
+    console.log(geoApi);
+    fetch(geoApi)
+    .then(function(response){
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        alert("the city you have entered is either not in the US or in the Database please choose another")
+        throw new Error("Can't Connect to API/City is invalid/something wrong: geo");
+      }
+    })
     .then(function (data){
+      console.log(data);
+      })
       function addHistory(data){};
       function displayToday (data){};
       function displayNextFive(data){};
-    })
-  }
-};
+    }
+  };
 
-function renderHisory(searchedHistory){
-};
+// function renderHisory(searchedHistory){
+// };
 
