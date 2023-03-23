d2357669db3a905789a97e627b9d513a71c554f8
NeelCheo <73446815+NeelCheo@users.noreply.github.com>
2023-03-22 20:27:55 -0400

    made all the data display propperly when needed and where needed

--- assets/script.js
+++ assets/script.js
@@ -3,68 +3,78 @@ var time = dayjs();
 var searchInput = $("#search-input");
 var submitBtn = $("#submit-button");
 var searchHistory = $("#search-history");
-var todayInfo = $("#today-info");
-var fiveDayInfo = $("#5-day-info");
+var historyBtn = $("*#historyBtn");
 //data storage variables
 var searchedHistory = [];
-var temp5Day = [];
-var wind5Day = [];
-var humididity5Day = [];
-var icon5Day = [];
-var cityName = ''
-var todayTemp = ''
-var todayWind = ''
-var todayHumididity = ''
-var todayIcon = '';
 if (localStorage.getItem("searchedHistory") != null) {
   searchedHistory = JSON.parse(localStorage.getItem("searchedHistory"));
+  displayInfo(searchedHistory[0]);
+} else{
+  displayInfo("Fairfield");
 }
 // renders the search history on load based on local storage
-// renderHistory(searchedHistory); !!!!!!!!!!!!!!!!!
+renderHistory();
+function renderHistory() {
+  if (localStorage.getItem("searchedHistory") === null) {
+    return;
+  } else {
+    searchHistory.html("");
+    for (let i = 0; i < searchedHistory.length; i++) {
+      let li = $("<button>");
+      li.attr("type", "button");
+      li.attr("class", "btn btn-primary btn-lg btn-block");
+      li.attr("id", "historyBtn");
+      li.text(searchedHistory[i]);
+      searchHistory.append(li);
+    }
+  }
+}
+searchHistory.on("click", "#historyBtn", function (event) {
+  console.log("yay");
+  displayInfo(this.textContent);
+});
 
-// on click function that will handles everything 
 //since the app is mainly displaying the info from the api
 submitBtn.on("click", function (event) {
   event.preventDefault();
   var city = searchInput.val().trim().toLowerCase();
   city = city.charAt(0).toUpperCase() + city.slice(1);
   if (city == "") {
-    return
-  } else if(searchedHistory.includes(city)){ //swaps history instead of spamming 
+    return;
+  } else if (searchedHistory.includes(city)) {
+    //swaps history instead of spamming
     var tmp = searchedHistory[0];
     var prevCity = searchedHistory.indexOf(city);
     delete searchHistory[prevCity];
-    searchedHistory[0]=searchedHistory[prevCity];
-    searchedHistory[prevCity]=tmp;
+    searchedHistory[0] = searchedHistory[prevCity];
+    searchedHistory[prevCity] = tmp;
     localStorage.setItem("searchedHistory", JSON.stringify(searchedHistory));
     displayInfo(city);
-  }else{
+    renderHistory();
+  } else {
     displayInfo(city);
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + ",US&appid=b117dcb8d3b6343f236365cabe485360&units=imperial")
-      .then(function (response) {
-        if (response.status === 200) {
-          addHistory(city);
-          function addHistory(city) {
-            if (localStorage.getItem("searchedHistory") === null) {
-              searchedHistory.push(city);
-              localStorage.setItem("searchedHistory", JSON.stringify(searchedHistory));
-            } else {
-              searchedHistory = JSON.parse(localStorage.getItem("searchedHistory"));
-              searchedHistory.unshift(city);
-              localStorage.setItem("searchedHistory", JSON.stringify(searchedHistory));
-            }
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + ",US&appid=b117dcb8d3b6343f236365cabe485360&units=imperial").then(function (response) {
+      if (response.status === 200) {
+        addHistory(city);
+        renderHistory();
+        function addHistory(city) {
+          if (localStorage.getItem("searchedHistory") === null) {
+            searchedHistory.push(city);
+            localStorage.setItem("searchedHistory", JSON.stringify(searchedHistory));
+          } else {
+            searchedHistory = JSON.parse(localStorage.getItem("searchedHistory"));
+            searchedHistory.unshift(city);
+            localStorage.setItem("searchedHistory", JSON.stringify(searchedHistory));
           }
         }
-      })
-    // renderHisory(); !!!!!!!!!!!!!!!!!
-    // function renderHisory(searchedHistory){}; !!!!!!!!!!!!!!!!!
-    //at end clear input !!!!!!!!!!!!!!!!
+      }
+    });
   }
 });
 
 //display the info from the api to the screen
 function displayInfo(city) {
-  // DISCLAIMER: it seems the weatherAPI doesnt need the coordinates and can use city name isntead 
+  // DISCLAIMER: it seems the weatherAPI doesnt need the coordinates and can use city name isntead
   var forecastApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",US&appid=b117dcb8d3b6343f236365cabe485360&units=imperial";
   var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",US&appid=b117dcb8d3b6343f236365cabe485360&units=imperial";
   dsiaplayApiData(forecastApi);
@@ -75,7 +85,7 @@ function displayInfo(city) {
         if (response.status === 200) {
           return response.json();
         } else {
-          alert("the city you have entered is either not in the US or in the Database please choose another")
+          alert("the city you have entered is either not in the US or in the Database please choose another");
           throw new Error("Can't Connect to API/City is invalid/something wrong: geo");
         }
       })
@@ -83,33 +93,30 @@ function displayInfo(city) {
         if (api == weatherApi) {
           displayWeather(data);
           function displayWeather(data) {
-            cityName = data.name;
-            todayTemp = data.main.temp;
-            todayWind = data.wind.speed;
-            todayHumididity = data.main.humidity;
-            todayIcon = data.weather[0].icon;
+            date.textContent = data.name + " (" + dayjs().format("DD/MM/YYYY") + ")";
+            temp.textContent = "Temp: " + data.main.temp + " F";
+            wind.textContent = "Wind: " + data.wind.speed + " MPH";
+            humidity.textContent = "Humidity: " + data.main.humidity + " %";
+            icon.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
           }
         } else if (api == forecastApi) {
-          var x = 3
-          var list = data.list;
-          temp5Day = [];
-          wind5Day = [];
-          humididity5Day = [];
-          icon5Day = [];
-          for (i = 0; i < list.length; i++) {
-            if (i == x) {
-              temp5Day.push(list[i].main.temp)
-              wind5Day.push(list[i].wind.speed)
-              humididity5Day.push(list[i].main.humidity)
-              icon5Day.push(list[i].weather[0].icon)
-              x = x + 8;
-            }
+          displayForecast(data);
+          function displayForecast(data) {
+            var x = 3;
+            var y = 0;
+            var list = data.list;
+            for (let i = 0; i < list.length; i++) {
+              if (i == x) {
+                date5[y].textContent = dayjs(list[i].dt_txt.split(" ", 1)).format("DD/MM/YYYY");
+                icon5[y].src = "http://openweathermap.org/img/wn/" + list[i].weather[0].icon + "@2x.png";
+                temp5[y].textContent = "Temp: " + list[i].main.temp + " F";
+                wind5[y].textContent = "Wind: " + list[i].wind.speed + " MPH";
+                humidity5[y].textContent = "Humidity: " + list[i].main.humidity + " %";
+                y++;
+                x = x + 8;
+              }}
           }
         }
-      })
-  };
-
-};
-
-
-
+      });
+  }
+}
